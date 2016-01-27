104b8851c467dc1263663b283575d04d4f18e805
Ziya SARIKAYA <sarikayaziya@gmail.com>
2016-01-27 21:38:08 +0200

    update sample code

--- sample/main.js
+++ sample/main.js
@@ -1,12 +1,16 @@
 import "babel-polyfill";
 
-import {Configuration} from '../src/index';
+import {Cluster, ClusterProvider, GuidGenerator, FrameworkProvider} from '../src/index';
 
 async function main() {
     try {
-        let configuration = new Configuration();
-        var uri = await configuration.getUri();
-        console.log(uri);
+        let clusterProvider = new ClusterProvider();
+        let frameworkProvider = new FrameworkProvider();
+        let guidGenerator = new GuidGenerator();
+        let cluster = new Cluster(clusterProvider, frameworkProvider, guidGenerator);
+
+        await cluster.bootstrap("customers", "v1");
+
     } catch (error) {
         console.error(error);
     }
