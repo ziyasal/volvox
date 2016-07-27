a87b67e7e54da71e0786979bd973a8fbb1652842
Ziya SARIKAYA <sarikayaziya@gmail.com>
2016-07-27 14:16:17 +0200

    export default *Volvox*

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import Volvox from './volvox';
-import Configuration from './configuration';
-import ServiceInformation from './service-information';
-import FrameworkProvider from './framework-provider';
+import Volvox from './volvox'
+import Configuration from './configuration'
+import ServiceInformation from './service-information'
+import FrameworkProvider from './framework-provider'
 
-import GuidGenerator from './util/guid-generator';
-import RandomGenerator from './util/random-generator';
-import ClusterProvider from './cluster-providers/cluster-provider';
+import GuidGenerator from './util/guid-generator'
+import RandomGenerator from './util/random-generator'
+import ClusterProvider from './cluster-providers/cluster-provider'
+
+export default Volvox
 
 export {
-    Volvox,
     Configuration,
     ServiceInformation,
     FrameworkProvider,
     ClusterProvider,
     GuidGenerator,
     RandomGenerator
-};
+}
